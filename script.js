4bfb4890bd09988895faeb8d39fc644819f2e12d
Nadeen Shilbayeh <nshilbay@uwo.ca>
2023-09-19 11:59:51 -0400

    Added the countries list in the JavaScript file

---
+++ script.js
@@ -0,0 +1,66 @@
+const countriesList =[
+    {
+        name: "Bahrain", currency: "BHD",
+    },
+    {
+        name: "Bosnia", currency: "BAM",
+    },
+    {
+        name: "Brunei", currency: "BND",
+    },
+    {
+        name: "Georgia", currency:"GEL",
+    },
+    {
+        name: "Greenland", currency:"DKK",
+    },
+    {
+        name: "Iceland", currency:"ISK",
+    },
+    {
+        name: "Mayotte", currency:"EUR",
+    },
+    {
+        name: "Micronesia", currency:"USD",
+    },
+    {
+        name: "New Caledonia", currency: "CFP",
+    }, 
+    {
+        name: "Saint Vincent Grenadines", currency: "XCD",
+    }, 
+    {
+        name: "Samoa", currency: "EUR",
+    }, 
+    {
+        name: "Sierra Leone", currency: "EUR",
+    }, 
+    {
+        name: "South Sudan", currency: "SSP",
+    }, 
+    {
+        name: "Suriname", currency: "SRD",
+    }, 
+    {
+        name: "Tajikistan", currency: "TJS",
+    }, 
+    {
+        name: "Timor-Leste", currency: "USD",
+    },
+    {
+        name: "Togo", currency: "XOF",
+    }, 
+    {
+        name: "Turkmenistan", currency: "TMT",
+    },
+    {
+        name: "Vanuatu", currency: "VUV",
+    },
+    {
+        name: "Wallis and Futuna", currency: "XPF",
+    },          
+]    
+
+function searchCountryUsingName(){
+    
+}

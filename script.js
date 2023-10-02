f8cc4f2ac39803a453725819a8f531a68b7d1622
Nadeen Shilbayeh <nshilbay@uwo.ca>
2023-10-02 12:00:44 -0400

    add the info of the countries in the countriesList

--- script.js
+++ script.js
@@ -1,63 +1,143 @@
 const countriesList =[
     {
-        name: "Bahrain", currency: "BHD",
+        name: "Bahrain", 
+        imgUrl: "images/Bahrain.png", 
+        currency: "BHD", 
+        regions: "Al 'Asimah, Al Janubiyah, Al Muharraq, Ash Shamaliyah", 
+        wikiUrl: "https://en.wikipedia.org/wiki/Bahrain"
     },
     {
-        name: "Bosnia", currency: "BAM",
+        name: "Bosnia",
+        imgUrl: "images/Bosnia.png",
+        currency: "BAM",
+        regions: "Brcko distrikt, Federacija Bosne i Hercegovine, Republika Srpska",
+        wikiUrl: "https://en.wikipedia.org/wiki/Bosnia_and_Herzegovina"
     },
     {
-        name: "Brunei", currency: "BND",
+        name: "Brunei",
+        imgUrl: "images/Brunei.png", 
+        currency: "BND",
+        regions: "Belait, Brunei-Muara, Temburong, Tutong",
+        wikiUrl: "https://en.wikipedia.org/wiki/Brunei"
     },
     {
-        name: "Georgia", currency:"GEL",
+        name: "Georgia",
+        imgUrl: "images/Georgia.png",
+        currency:"GEL",
+        regions: "Haut-Ogooue, Ngounie, Nyanga, Ogooue-Maritime, Woleu-Ntem",
+        wikiUrl: "https://en.wikipedia.org/wiki/Georgia"
     },
     {
-        name: "Greenland", currency:"DKK",
+        name: "Greenland",
+        imgUrl: "images/Greenland.png",
+        currency:"DKK",
+        regions: "Avannaata Kommunia, Kommune Kujalleq, Kommune Qeqertalik, Kommuneqarfik Sermersooq, Qeqqata Kommunia",
+        wikiUrl: "https://en.wikipedia.org/wiki/Greenland"
     },
     {
-        name: "Iceland", currency:"ISK",
+        name: "Iceland",
+        imgUrl: "images/Iceland.png", 
+        currency:"ISK",
+        regions: "Austurland, Hofudborgarsvaedi, Nordurland eystra. Nordurland vestra, Sudurland, Sudurnes, Vestfirdir, Vesturland",
+        wikiUrl: "https://en.wikipedia.org/wiki/Iceland"
     },
     {
-        name: "Mayotte", currency:"EUR",
+        name: "Mayotte",
+        imgUrl: "images/Mayotte.png", 
+        currency:"EUR",
+        regions: "Bandraboua, Bandrele, Mamoudzou, Ouangani",
+        wikiUrl: "https://en.wikipedia.org/wiki/Mayotte"
     },
     {
-        name: "Micronesia", currency:"USD",
+        name: "Micronesia",
+        imgUrl: "images/Micronesia.png", 
+        currency:"USD",
+        regions: "Chuuk, Kosrae, Pohnpei, Yap",
+        wikiUrl: "https://en.wikipedia.org/wiki/Micronesia"
     },
     {
-        name: "New Caledonia", currency: "CFP",
+        name: "New Caledonia",
+        imgUrl: "images/New Caledonia.png", 
+        currency: "CFP",
+        regions: "Province Nord, Province Sud",
+        wikiUrl: "https://en.wikipedia.org/wiki/New_Caledonia"
     }, 
     {
-        name: "Saint Vincent Grenadines", currency: "XCD",
+        name: "Saint Vincent Grenadines",
+        imgUrl: "images/Saint Vincent Grenadines.png",
+        currency: "XCD",
+        regions: "Charlotte,  Grenadines, Saint George, Saint Patrick",
+        wikiUrl: "https://en.wikipedia.org/wiki/Saint_Vincent_and_the_Grenadines"
     }, 
     {
-        name: "Samoa", currency: "EUR",
+        name: "Samoa",
+        imgUrl: "images/Samoa.png", 
+        currency: "EUR",
+        regions: "Atua, Fa'asaleleaga, Tuamasaga",
+        wikiUrl: "https://en.wikipedia.org/wiki/Samoa"
     }, 
     {
-        name: "Sierra Leone", currency: "EUR",
+        name: "Sierra Leone",
+        imgUrl: "images/Sierra Leone.png", 
+        currency: "EUR",
+        regions: "Eastern, Northern, Southern, Western Area",
+        wikiUrl: "https://en.wikipedia.org/wiki/Sierra_Leone"
     }, 
     {
-        name: "South Sudan", currency: "SSP",
+        name: "South Sudan",
+        imgUrl: "images/South Sudan.png",
+        currency: "SSP",
+        regions: "Central Equatoria, Eastern Equatoria, Jonglei Lakes, Northern Bahr el Ghazal, Upper Nile, Western Equatoria",
+        wikiUrl:  "https://en.wikipedia.org/wiki/South_Sudan"
     }, 
     {
-        name: "Suriname", currency: "SRD",
+        name: "Suriname",
+        imgUrl: "images/Suriname.png", 
+        currency: "SRD",
+        regions: "Brokopondo, Commewijne, Nickerie, Para, Paramaribo, Sipaliwini, Wanica",
+        wikiUrl: "https://en.wikipedia.org/wiki/Suriname"
     }, 
     {
-        name: "Tajikistan", currency: "TJS",
+        name: "Tajikistan",
+        imgUrl: "images/Tajikistan.png", 
+        currency: "TJS",
+        regions: "Dushanbe, Khatlon, Nohiyahoi, Tobei, Jumhuri, Sughd",
+        wikiUrl: "https://en.wikipedia.org/wiki/Tajikistan"
     }, 
     {
-        name: "Timor-Leste", currency: "USD",
+        name: "Timor-Leste", 
+        imgUrl: "images/Timor-Leste.png",
+        currency: "USD",
+        regions: "Ainaro, Bobonaro, Cova, Lima, Dili, Liquica",
+        wikiUrl: "https://en.wikipedia.org/wiki/East_Timor"
     },
     {
-        name: "Togo", currency: "XOF",
+        name: "Tonga",
+        imgUrl: "images/Togo.png",
+        currency: "XOF",
+        regions: "Ha'apai, Niuas, Niuas",
+        wikiUrl: "https://en.wikipedia.org/wiki/Togo"
     }, 
     {
-        name: "Turkmenistan", currency: "TMT",
+        name: "Turkmenistan",
+        imgUrl: "images/Turkmenistan.png", 
+        currency: "TMT",
+        regions: "Ahal, Balkan, Dasoguz, Lebap, Mary",
+        wikiUrl: "https://en.wikipedia.org/wiki/Turkmenistan"
     },
     {
-        name: "Vanuatu", currency: "VUV",
+        name: "Vanuatu",
+        imgUrl: "images/Vanuatu.png", 
+        currency: "VUV",
+        regions: "Shefa, Tafea, Torba",
+        wikiUrl: "https://en.wikipedia.org/wiki/Vanuatu"
     },
     {
-        name: "Wallis and Futuna", currency: "XPF",
+        name: "Wallis and Futuna",
+        imgUrl: "images/Wallis and Futuna.png", 
+        currency: "XPF",
+        regions: "Sigave, Uvea",
+        wikiUrl: "https://en.wikipedia.org/wiki/Wallis_and_Futuna"
     },          
 ]    
 
@@ -146,5 +226,22 @@ function dynamicallyDisplayMatches(results){
     while (matchesContainer.firstChild){
         matchesContainer.removeChild(matchesContainer.firstChild)
     }
+
+    //the if statement is to hide the container if there are no matches
+    if (results.length === 0){
+        matchesContainer.style.display == "none";
+    }
+    else{ //show container if there are matches
+        matchesContainer.style.display == "block";
+
+        for (const country of results){
+            const countryElement = document.createElement("div");
+
+            const img = document.createElement("img");
+            img.src - country.
+        }
+    }
+
+
 
 }
